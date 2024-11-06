7addf252c5aa13126fc63b152ec64e6456b13c2e
varinmehta <mehtavarin2004@gmail.com>
2024-11-06 19:29:45 +0530

    commit

--- app.js
+++ app.js
@@ -13,14 +13,4 @@ connectDB(process.env.MONGO_URL);
 // Middleware setup
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-    cors({
-        origin: "https://read-sync-varinmehta-varin-mehtas-projects.vercel.app",
-    })
-); // If needed for handling CORS
-
-// Route setup
-app.use("/api", user); // Prefix routes with "/api" for consistency with Vercel's routing
-
-// Export the app for Vercel's serverless function handler
 module.exports = app;
